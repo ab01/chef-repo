5212eb0630af9dc6674c51f4f9bf261b9d33fa8c
Ankit Bhalla <ab@xyz.com>
2018-03-11 14:01:16 +0000

    adding wrapper supermarket cookbook

--- cookbooks/my_supermarket_wrapper/recipes/default.rb
+++ cookbooks/my_supermarket_wrapper/recipes/default.rb
@@ -9,6 +9,6 @@ app = data_bag_item('apps', 'supermarket')
 node.override['supermarket_omnibus']['chef_server_url'] = app['chef_server_url']
 node.override['supermarket_omnibus']['chef_oauth2_app_id'] = app['chef_oauth2_app_id']
 node.override['supermarket_omnibus']['chef_oauth2_secret'] = app['chef_oauth2_secret']
-node.override['supermarket_omnibus']['config']['fqdn'] = your_node_public_ip
+node.override['supermarket_omnibus']['config']['fqdn'] = '54.235.8.125'
 
 include_recipe 'supermarket-omnibus-cookbook'
